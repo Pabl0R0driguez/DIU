f4a8ace7040f8e43f310e4afec658b76eb4acdbd
Pabl0R0driguez <163997031+Pabl0R0driguez@users.noreply.github.com>
2024-10-04 14:42:33 +0200

    Add files via upload

--- DIU/JavaFXML2/src/main/java/com/example/demo/javafxml2/HelloApplication.java
+++ DIU/JavaFXML2/src/main/java/com/example/demo/javafxml2/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo.javafxml2;
 
+import com.example.demo.javafxml2.controller.HelloController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -16,7 +17,26 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
 
+        FXMLLoader fxmlLoader2 = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Stage stage2 = new Stage();
+        Scene scene2 = new Scene(fxmlLoader2.load());
+
+
+        stage2.setTitle("Hello!");
+        stage2.setScene(scene2);
+        stage2.show();
+
+        HelloController clase1 = fxmlLoader.getController();
+        HelloController clase2 = fxmlLoader.getController();
+
+        clase2.pulsaciones.bindBidirectional(clase1.pulsaciones);
+
+
+
+
+
     }
+
 
     public static void main(String[] args) {
         launch();
--- DIU/JavaFXML2/src/main/java/com/example/demo/javafxml2/controller/HelloController.java
+++ DIU/JavaFXML2/src/main/java/com/example/demo/javafxml2/controller/HelloController.java
@@ -1,67 +1,93 @@
 package com.example.demo.javafxml2.controller;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.scene.input.KeyCode;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 public class HelloController {
+	String nuevoTexto = "";
+	int nuevoNumero = 0;
+	int textoNumero = 0;
+
 	@FXML
 	private ProgressBar barra;
 	@FXML
 	private TextField texto;
 	@FXML
-	private Button incrementar,actualizar,aplicar,reducir;
+	private Button incrementar, actualizar, aplicar, reducir;
 	@FXML
 	private Label contador;
 
 
-	private int pulsaciones = 0;
+	public SimpleIntegerProperty pulsaciones = new SimpleIntegerProperty (0);
 
 	@FXML
 	public void incrementar() {
-		pulsaciones++;
-		contador.setText(Integer.toString(pulsaciones));
-		barra.setProgress(pulsaciones / 10.0);
-	}
-
-	@FXML
-	public void actualizar() {
-		contador.setText("Contador actualizado: " + pulsaciones);
+		pulsaciones.set(pulsaciones.get()+1);
+		contador.setText(String.valueOf(pulsaciones));
+		barra.setProgress(pulsaciones.get() / 10.0);
 	}
 
 	@FXML
 	public void reducir() {
-			pulsaciones--;
-			contador.setText(Integer.toString(pulsaciones));
-			barra.setProgress(pulsaciones / 10.0);
+		pulsaciones.set(pulsaciones.get()-1);
+		contador.setText(String.valueOf(pulsaciones));
+		barra.setProgress(pulsaciones.get() / 10.0);
 
 	}
 
-	public void cambiarValor(){
-
+	@FXML
+	public void actualizar() {
+		pulsaciones.set(pulsaciones.get());
+		contador.setText(String.valueOf(pulsaciones));
+		barra.setProgress(pulsaciones.get() / 10.0);
 	}
 
-	public void aplicar(ActionEvent actionEvent) {
-		//Obtengo el texto del textField
-		String nuevoTexto = texto.getText();
-		//pasamos el texto recogio a entero
-		int nuevoNumero = Integer.parseInt(nuevoTexto);
-		//Actualizamso el contador con el nuevo numero
-		contador.setText(Integer.toString(nuevoNumero));
-		if(nuevoNumero>=25){
-			barra.setProgress(pulsaciones / 10.0);
-		}
-		else if (nuevoNumero < 25) {
-			barra.setProgress(pulsaciones/10.0);
-		}
-		else if (nuevoNumero == 50) {
-			barra.setProgress(pulsaciones/10.0);
-		}
-		else if (nuevoNumero == 0) {
-			barra.setProgress(pulsaciones/10.0);
-		}
+
+	//Botón para actualizar barra
+/*//	public void aplicar(ActionEvent actionEvent) {
+//		//Obtengo el texto del textField
+//		nuevoTexto = texto.getText();
+//		//pasamos el texto recogio a entero
+//		pulsaciones = Integer.parseInt(nuevoTexto);
+//		//Actualizamso el contador con el nuevo numero
+//		contador.setText(Integer.toString(pulsaciones));
+//
+//		//Actual
+//		contador.setText(Integer.toString(pulsaciones));
+//		barra.setProgress(pulsaciones / 10.0);
+//
+//	}*/
+
+	pulsaciones.addListener((obs, oldValue, newValue) -> {
+		pulsaciones.set(Integer.parseInt(texto.getText()));
+		contador.setText(String.valueOf(pulsaciones));
+		barra.setProgress(pulsaciones.get() / 10.0);
+	});
+	@FXML
+	public void initialize() {
+		// Escuchar el evento de la tecla Enter en el TextField
+		texto.setOnKeyPressed(event -> {
+			if (event.getCode().getName().equals("Enter")) {
+				// pasamos el tesxtField a entero y actualizamos el label y la barra
+			pulsaciones.set(Integer.parseInt(texto.getText()));
+				contador.setText(String.valueOf(pulsaciones));
+				barra.setProgress(pulsaciones.get() / 10.0);
+
+			}
+		});
 	}
+
 }
+
+
+
+
+
