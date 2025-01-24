c4962eb29e8ad61dbf330b712a015b7851cb9c0c
Pabl0Rodriguez <pablorodseg@gmail.com>
2025-01-24 14:30:23 +0100

    s

--- 2TRIMESTRE/React/Tutorials/src/main/java/com/example/tutorials/Tutorials/TutorialsApplication.java
+++ 2TRIMESTRE/React/Tutorials/src/main/java/com/example/tutorials/Tutorials/TutorialsApplication.java
@@ -1,4 +1,4 @@
-package com.example.tutorials;
+package com.example.tutorials.Tutorials;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,5 @@ public class TutorialsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TutorialsApplication.class, args);
 	}
--
+
 }
