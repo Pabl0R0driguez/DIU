9ce9f217eca61efaf3739917327797dcf1688609
Pabl0Rodriguez <pablorodseg@gmail.com>
2025-02-11 13:40:14 +0100

    s

--- 2TRIMESTRE/ProyectoFinal2T/FrontAgenda/src/App.js
+++ 2TRIMESTRE/ProyectoFinal2T/FrontAgenda/src/App.js
@@ -1,50 +1,60 @@
 import React, { Component } from "react";
 import { Switch, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css"; // Make sure Bootstrap 5 is installed
+import "./App.css"; // Import your custom CSS
 import TutorialsList from "./components/tutorials-list.component";
 import AddTutorial from "./components/add-tutorial.component";
 import EditTutorial from "./components/edit-tutorial.component";
-import Login  from "./components/login";
-
+import Login from "./components/login";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <Link to={"/tutorials"} className="navbar-brand">
-            Tutoriales
-          </Link>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/tutorials"} className="nav-link">
-                Tutorials
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/add"} className="nav-link">
-                Add
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link to={"/login"} className="nav-link">
-                Login 
-              </Link>
-            </li>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container">
+            <Link to={"/tutorials"} className="navbar-brand">
+              Tutoriales
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <Link to={"/tutorials"} className="nav-link">
+                    Tutorials
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={"/add"} className="nav-link">
+                    Add
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={"/login"} className="nav-link">
+                    Login
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
 
         <div className="container mt-3">
           <Switch>
-          {/*El en switch se renderizarán todas los compoentes cuta URL coicidan con la activa*/}
             <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
-            <Route exact path="/add" component={AddTutorial} /> 
-         <Route path="/tutorials/:id" component={EditTutorial} /> 
-         <Route path="/login" component={Login} /> 
-
-
+            <Route exact path="/add" component={AddTutorial} />
+            <Route path="/tutorials/:id" component={EditTutorial} />
+            <Route path="/login" component={Login} />
           </Switch>
         </div>
       </div>
